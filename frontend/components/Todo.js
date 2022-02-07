17f2e43d5ad0084548c8eb09efa704bb2c43fc5d
Chris Kelley <chriskelley85@gmail.com>
2022-02-07 15:17:33 -0600

    add and clear input

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 export default class Todo extends React.Component {
+  handleClick = () => {
+    this.props.handleRemoveComplete(props.task);
+  }
+
   render() {
     return (
-      <div>
-        Todo Proj
+      <div onClick={this.handleClick}>
+        <p>{this.props.task}</p>
       </div>
     )
   }
